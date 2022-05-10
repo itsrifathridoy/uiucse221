4fcbf641f31c815a698123f73aea033925824d4e
Rifat Hridoy <rifathridoy@hotmail.com>
2022-05-11 01:16:36 +0600

    add operators problem

--- ICS/Personal Practice/test.c
+++ ICS/Personal Practice/test.c
@@ -1,41 +1,12 @@
-#include <stdio.h>
-#include<math.h>
+#include<stdio.h>
+int main()
+    {
+       char op;
+       int a,b;
 
-int main() {
+       scanf("%d %c %d",&a,&op,&b);
 
-    float N;
-    scanf("%f",&N);
-    printf("NOTAS:\n");
-    printf("%d nota(s) de R$ 100.00\n",(int)N/100);
-    N=fmodf(N,100);
-    printf("%d nota(s) de R$ 50.00\n",(int)N/50);
-    N=fmodf(N,50);
-    printf("%d nota(s) de R$ 20.00\n",(int)N/20);
-    N=fmodf(N,20);
-    printf("%d nota(s) de R$ 10.00\n",(int)N/10);
-    N=fmodf(N,10);
-    printf("%d nota(s) de R$ 5.00\n",(int)N/5);
-    N=fmodf(N,5);
-    printf("%d nota(s) de R$ 2.00\n",(int)N/2);
-    N=fmodf(N,2);
+       printf("%d %c %d",a, op, b);
 
 
-    printf("MOEDAS:\n");
-    printf("%d moeda(s) de R$ 1.00\n",(int)N/1);
-    N=fmodf(N,1);
-    printf("reminder: %f\n",N);
-    printf("%d moeda(s) de R$ 0.50\n",(int)(N/0.50));
-    printf("reminder: %f\n",N);
-    N=fmodf(N,0.5);
-    printf("reminder: %f\n",N);
-    printf("%d moeda(s) de R$ 0.25\n",(int)(N/0.25));
-    N=fmodf(N,0.25);
-    printf("reminder: %f\n",N);
-    printf("%d moeda(s) de R$ 0.10\n",(int)(N/0.10));
-    N=fmodf(N,0.10);
-    printf("%d moeda(s) de R$ 0.05\n",(int)(N/0.05));
-    N=fmodf(N,0.05);
-    printf("%d moeda(s) de R$ 0.01\n",(int)(N/0.01));
-
-    return 0;
-}
+    }
