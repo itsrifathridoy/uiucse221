3c74885418cfdee1a96413ac361488abb6542410
royprogramer <sampadroykoushik01@gmail.com>
2022-05-11 02:26:37 +0600

    oparetor

---
+++ ICS/Practice 1/02. Operators/operator 03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,b,X;
+    printf("Enter two number:");
+    scanf("%f%f",&a,&b);
+    X =(3.31 * a*a + 2.01 * b*b*b)/(7.16*b*b + 2.01*a*a*a);
+    printf("The answer is=%f",X);
+
+    return 0;
+}
---
+++ ICS/Practice 1/02. Operators/operator 04.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+   int X;
+   scanf("%d",&X);
+   printf("X++:%d\n",X++);
+   printf("++X:%d\n",X--);
+   printf("X--:%d\n",X);
+   printf("--X:%d\n",--X);
+    return 0;
+}
+
