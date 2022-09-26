2cd1c89a7245ee09cef93e006319b08bce6fec6d
Rifat Hridoy <rifathridoy@hotmail.com>
2022-09-26 15:52:22 +0600

    add some code

--- SPL/Practice Problem/2D Array/1.c
+++ SPL/Practice Problem/2D Array/1.c
@@ -7,12 +7,6 @@ int main()
         for(int j=0;j<3;j++)
         {
             scanf("%d",&arr[i][j]);
-        }
-    }
-    for(int i=0;i<3;i++)
-    {
-        for(int j=0;j<3;j++)
-        {
             printf("%d ",arr[i][j]);
         }
         printf("\n");
