107954a89b81b0aec7bbbdce3f358aea66f4b680
Rifat Hridoy <rifathridoy@hotmail.com>
2022-09-27 03:49:23 +0600

    add some code

--- SPL/Practice Problem/Mixed Problem/10.c
+++ SPL/Practice Problem/Mixed Problem/10.c
@@ -1,7 +1,34 @@
 #include<stdio.h>
 struct patient
+{
+    char name[100];
+    char id[100];
+    int height;
+    int weight;
+    int age;
+};
 int main()
 {
+    int n=2;
+    struct patient patients[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf(" %[^\n]s",patients[i].name);
+        scanf(" %[^\n]s",patients[i].id);
+        scanf("%d",&patients[i].height);
+        scanf("%d",&patients[i].weight);
+        scanf("%d",&patients[i].age);
+    }
+    int max= patients[0].height;
+    int maxIndex=0;
+    for(int i=1;i<n;i++)
+    {
+        if(max<patients[i].height)
+        {
+            maxIndex=i;
+        }
+    }
+    printf("%s\n",patients[maxIndex].id);
     return 0;
 }
 
