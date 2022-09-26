5ab7accc274e566650dd25d93c292c68e1bd0a5b
Rifat Hridoy <rifathridoy@hotmail.com>
2022-09-26 15:24:03 +0600

    add some code

--- SPL/Practice Problem/2D Array/14.c
+++ SPL/Practice Problem/2D Array/14.c
@@ -1,1 +1,46 @@
+#include<stdio.h>
+int main()
+{
+    int m,n;
+    scanf("%d %d",&m,&n);
+    int arr[m][n];
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int count=0;
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            for(int k=0;k<m;k++)
+            {
+                for(int l=0;l<n;l++)
+                {
+                    if(arr[i][j]==arr[k][l])
+                    {
+                        if(count>0)
+                        {
+                            arr[k][l]=-1;
+                        }
+                        count++;
+                    }
+                }
+            }
+            count=0;
+        }
+    }
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf(" %d",arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
